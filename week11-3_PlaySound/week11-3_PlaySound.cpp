4dd42b1da4369d908f7d53b22912579f0f664456
ww218 <g8wlc681208@gmail.com>
2023-04-26 12:05:04 +0800

    week11

---
+++ week11-3_PlaySound/week11-3_PlaySound.cpp
@@ -0,0 +1,7 @@
+#include <windows.h>
+int main()
+{
+    PlaySound("C:\\Users\\Administrator\\Desktop\\do_re_mi\\do.wav",NULL,SND_SYNC);
+    PlaySound("C:\\Users\\Administrator\\Desktop\\do_re_mi\\re.wav",NULL,SND_SYNC);
+    PlaySound("C:\\Users\\Administrator\\Desktop\\do_re_mi\\mi.wav",NULL,SND_SYNC);
+}
