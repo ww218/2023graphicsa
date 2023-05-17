a6b8040d5a5da509a010a9495c305551d3c15dcc
ww218 <g8wlc681208@gmail.com>
2023-05-17 12:08:15 +0800

    week14

--- Final_Project/main.cpp
+++ Final_Project/main.cpp
@@ -5,14 +5,14 @@ GLMmodel * head =NULL;///week13-1
 GLMmodel * uparmR =NULL;///week13-1
 GLMmodel * lowarmR =NULL;///week13-1
 GLMmodel * body =NULL;///week13-1
-int show[4]={1,1,1,1};///week13-3
-int ID = 0;///week14 3-1 //0頭 1身體 2上手臂 3下手臂
-void keyboard(unsigned char key,int x, int y){///week13-3
+int show[4]={1,1,1,1};///week13-3///week14 3-2
+int ID = 3;///week14-3-1 //0頭 1身體 2上手臂 3下手臂///week14 3-2
+void keyboard(unsigned char key,int x, int y){
     if(key=='0') ID = 0;///week14 3-1
     if(key=='1') ID = 1;///week14 3-1
     if(key=='2') ID = 2;///week14 3-1
     if(key=='3') ID = 3;///week14 3-1
-    ///if(key=='0') show[0] = !show[0];
+    ///if(key=='0') show[0] = !show[0];///week13-3
     ///if(key=='1') show[1] = !show[1];
     ///if(key=='2') show[2] = !show[2];
     ///if(key=='3') show[3] = !show[3];
@@ -34,26 +34,40 @@ void display()
             lowarmR = glmReadOBJ("model/lowarmR.obj");///week13-3
             ///glmUnitize(body);///week13-1
         }
-        if(ID==0)glColor3f(1,0,0);///week14 3-1
-        else glColor3f(1,1,1);///week14 3-1
-        if(show[0]) glmDraw(body, GLM_MATERIAL);///week13-1
+        if(ID==0)glColor3f(1,0,0);///week14-3-1
+        else glColor3f(1,1,1);///week14-3-1
+        if(show[0]) glmDraw(head, GLM_MATERIAL);///week13-1
 
-        if(ID==1)glColor3f(1,0,0);///week14 3-1
-        else glColor3f(1,1,1);///week14 3-1
-        if(show[1]) glmDraw(head, GLM_MATERIAL);///week13-2
+        if(ID==1)glColor3f(1,0,0);///week14-3-1
+        else glColor3f(1,1,1);///week14-3-1
+        if(show[1]) glmDraw(body, GLM_MATERIAL);///week13-2
 
         glPushMatrix();///week13-4
-            glTranslatef(teapotX,teapotY,0);///week13-4
-
-            if(ID==2)glColor3f(1,0,0);///week14 3-1
-            else glColor3f(1,1,1);///week14 3-1
+            glTranslatef(-1.200356,+0.466667,0);///week14-3-2
+            //glTranslatef(teapotX,teapotY,0);
+            glRotatef(angle,0,0,1);///week14-3-2 TRT建出來
+            glTranslatef(1.200356,-0.466667,0);///week14-3-2
+            if(ID==2)glColor3f(1,0,0);///week14-3-1
+            else glColor3f(1,1,1);///week14-3-1
             if(show[2]) glmDraw(uparmR, GLM_MATERIAL);///week13-3
+
+            glPushMatrix();
+                glTranslatef(-1.946666,0.126667,0);///幫忙找出TRT移動的值///week14-3-3
+                glRotatef(angle,0,0,1);///week14-3-2 TRT建出來
+                glTranslatef(1.946666,-0.126667,0);///week14-3-2
+
+                if(ID==3)glColor3f(1,0,0);///week14-3-1
+                else glColor3f(1,1,1);///week14-3-1
+                if(show[3]) glmDraw(lowarmR, GLM_MATERIAL);///week13-3
+            glPopMatrix();
+
         glPopMatrix();///week13-4
 
-        if(ID==3)glColor3f(1,0,0);///week14 3-1
-        else glColor3f(1,1,1);///week14 3-1
-        if(show[3]) glmDraw(lowarmR, GLM_MATERIAL);///week13-3
+
     glPopMatrix();
+
+    glColor3f(0,1,0);///week14-3-2放個小茶壺在中心當參考點
+    glutSolidTeapot(0.02);///week14-3-2
     glutSwapBuffers();
 }
 int oldX=0,oldY=0;///week13-4
@@ -62,10 +76,10 @@ void motion (int x,int y){///week13-4
     teapotY-= (y-oldY)/150.0;
     oldX = x;
     oldY = y;
+    angle= x;///week14-3-2
     printf("glTranslatef(%f,%f,0);\n",teapotX,teapotY);
     glutPostRedisplay();
 }
-
 void mouse(int button,int state,int x,int y)
 {
     if(state==GLUT_DOWN){
@@ -90,7 +104,7 @@ int main(int argc, char *argv[])
 {
 	glutInit(&argc, argv);
 	glutInitDisplayMode( GLUT_DOUBLE | GLUT_DEPTH);
-	glutCreateWindow("week13");
+	glutCreateWindow("week14");
 
 	glutDisplayFunc(display);
 	glutMouseFunc(mouse);
